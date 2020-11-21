0a533d7646d4fe9e8cd2d762ee6aaa4bb37aedd2
Josephshitandi <jsphshtnd@gmail.com>
2020-11-21 14:44:31 +0300

    Modifies home.html, adding avater

--- instagram/views.py
+++ instagram/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
 from django.contrib.auth.decorators import login_required
+from .models import Image,Comment,Profile
 
 # Create your views here.
 def index(request):
     date = dt.date.today()
-   
-    return render(request, 'home.html', {"date": date})
+    images = Image.objects.all()
+    return render(request, 'home.html', {"date": date,"images":images})
 
